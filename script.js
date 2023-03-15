be83cc87dd5ace429449a6c2dcea5e395cce60a8
Mohanekm2001 <124660796+Mohanekm2001@users.noreply.github.com>
2023-03-15 10:07:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 function threeSum(arr, target) {
-// write your code here
-  
-}
+      arr.sort((a, b) => a - b);
+      let closestSum = Infinity;
 
-module.exports = threeSum;
+      for (let i = 0; i < arr.length - 2; i++) {
+        let left = i + 1;
+        let right = arr.length - 1;
+
+        while (left < right) {
+          const sum = arr[i] + arr[left] + arr[right];
+
+          if (sum === target) {
+            return sum;
+          }
+
+          if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+            closestSum = sum;
+          }
+
+          if (sum < target) {
+            left++;
+          } else {
+            right--;
+          }
+        }
+      }
+
+      return closestSum;
+    }
+
+    function findSum() {
+      const arr = document.getElementById("arr").value.split(" ").map(Number);
+      const target = Number(document.getElementById("target").value);
+      const result = threeSum(arr, target);
+
+      document.getElementById("result").innerHTML = `Closest Sum: ${result}`;
+    }
