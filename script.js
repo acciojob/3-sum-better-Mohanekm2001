573a5ca18ec53e97a1aa91989b14883f63199068
Mohanekm2001 <mohan2001ekm@gmail.com>
2023-04-18 10:17:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-function threeSum() {
-    const S = document.getElementById("inputArray").value.split(",").map(Number);
-    const target = Number(document.getElementById("target").value);
-
-    let closestSum = Infinity;
-
-    for (let i = 0; i < S.length - 2; i++) {
-        for (let j = i + 1; j < S.length - 1; j++) {
-            for (let k = j + 1; k < S.length; k++) {
-                const sum = S[i] + S[j] + S[k];
-                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-                    closestSum = sum;
-                }
-            }
-        }
+function threeSumClosest(nums, target) {
+  nums.sort((a, b) => a - b);
+  let closestSum = nums[0] + nums[1] + nums[2];
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1, right = nums.length - 1;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        return closestSum;
+        // If sum is equal to target, no need to continue
+      }
     }
+  }
+  return closestSum;
+}
 
-    document.getElementById("result").innerHTML = `The sum closest to ${target} is ${closestSum}.`;
+function handleSubmit() {
+  const numsInput = document.getElementById('nums');
+  const targetInput = document.getElementById('target');
+  const numsArr = numsInput.value.split(',').map(num => parseInt(num));
+  const target = parseInt(targetInput.value);
+  const closestSum = threeSumClosest(numsArr, target);
+  const resultDiv = document.getElementById('result');
+  resultDiv.textContent = `The sum closest to ${target} is ${closestSum}.`;
 }
